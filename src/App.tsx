8d09aa776f6baa41c07ebd83d09b7a42c355a825
Kurono7 <omar.aponte12@gmail.com>
2023-02-16 11:23:12 -0500

    feat: Create index.html

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import Header from 'components/Header'
+
+const App: React.FC = () => <Header />
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return <div>Hola Omar Orlando</div>
+}
+
+export default Header
